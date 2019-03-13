de692676c0540cd536451d6581c843aa450a9fbe
falsy <me@falsy.me>
2019-03-14 06:56:15 +0900

    insert sample code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-document.getElementById('push-state').addEventListener('click', () => {
-	history.pushState({data: 'pushState'}, '', '/push-state');
+document.getElementById('state').innerText = JSON.stringify(history.state);
+
+document.getElementById('push-state1').addEventListener('click', () => {
+	history.pushState({data: 'pushState1'}, '', '/push-state1');
+});
+document.getElementById('push-state2').addEventListener('click', () => {
+	history.pushState({data: 'pushState2'}, '', '/push-state2');
+});
+document.getElementById('push-state3').addEventListener('click', () => {
+	history.pushState({data: 'pushState3'}, '', '/push-state3');
+});
+document.getElementById('replace-state').addEventListener('click', () => {
+	history.replaceState({data: 'replaceState'}, '', '/replace-state');
+});
+window.addEventListener('popstate', () => {
+  document.getElementById('state').innerText = JSON.stringify(history.state);
 });
